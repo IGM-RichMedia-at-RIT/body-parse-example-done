32ac0ba6b814844a481294fc24d326a29db4877e
Austin Willoughby <arwigm@rit.edu>
2022-01-17 12:40:36 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -11,42 +11,62 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 //handle POST requests
 const handlePost = (request, response, parsedUrl) => {
-  //if post is to /addUser (our only POST url)
-  if (parsedUrl.pathname === '/addUser') {
-    const res = response;
-
-    //uploads come in as a byte stream that we need 
-    //to reassemble once it's all arrived
-    const body = [];
-
-    //if the upload stream errors out, just throw a
-    //a bad request and send it back 
-    request.on('error', (err) => {
-      console.dir(err);
-      res.statusCode = 400;
-      res.end();
-    });
-
-    //on 'data' is for each byte of data that comes in
-    //from the upload. We will add it to our byte array.
-    request.on('data', (chunk) => {
-      body.push(chunk); 
-    });
-
-    //on end of upload stream. 
-    request.on('end', () => {
-      //combine our byte array (using Buffer.concat)
-      //and convert it to a string value (in this instance)
-      const bodyString = Buffer.concat(body).toString();
-      //since we are getting x-www-form-urlencoded data
-      //the format will be the same as querystrings
-      //Parse the string into an object by field name
-      const bodyParams = query.parse(bodyString);
-
-      //pass to our addUser function
-      jsonHandler.addUser(request, res, bodyParams);
-    });
+  
+  //If they go to /addUser
+  if(parsedUrl.pathname === '/addUser') {
+    
+    //Call our below parseBody handler, and in turn pass in the
+    //jsonHandler.addUser function as the handler callback function.
+    parseBody(request, response, jsonHandler.addUser);
   }
+};
+
+//Recompiles the body of a request, and then calls the
+//appropriate handler once completed. Some request methods
+//(like POST) send their request body in pieces or chunks. This
+//is in contrast to something like a GET request, where the entire
+//request always comes in as one packet. In order to use a POST
+//request, we need to have the entire request body before proceeding.
+//This function will reassemble the body and then handle the request.
+//The "handler" parameter is the request handler function to call after
+//we have the request completely reassembled.
+const parseBody = (request, response, handler) => {
+  
+  //The request will come in in pieces. We will store those pieces in this
+  //body array.
+  const body = [];
+
+  //The body reassembly process is event driven, much like when we are streaming
+  //media like videos, etc. We will set up a few event handlers. This first one
+  //is for if there is an error. If there is, write it to the console and send
+  //back a 400-Bad Request error to the client.
+  request.on('error', (err) => {
+    console.dir(err);
+    response.statusCode = 400;
+    response.end();
+  });
+
+  //The second possible event is the "data" event. This gets fired when we
+  //get a piece (or "chunk") of the body. Each time we do, we will put it in
+  //the array. We will always recieve these chunks in the correct order.
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  });
+
+  //The final event is when the request is finished sending and we have recieved
+  //all of the information. When the request "ends", we can proceed. Turn the body
+  //array into a single entity using Buffer.concat, then turn that into a string.
+  //With that string, we can use the querystring library to turn it into an object
+  //stored in bodyParams. We can do this because we know that the client sends
+  //us data in X-WWW-FORM-URLENCODED format. If it was in JSON we could use JSON.parse.
+  request.on('end', () => {
+    const bodyString = Buffer.concat(body).toString();
+    const bodyParams = query.parse(bodyString);
+
+    //Once we have the bodyParams object, we will call the handler function. We then
+    //proceed much like we would with a GET request.
+    handler(request, response, bodyParams);
+  });
 };
 
 //handle GET requests
